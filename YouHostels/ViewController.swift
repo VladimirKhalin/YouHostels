a9a4f042acdacae47ba1a3b4ccbbfee8b012fbd6
VK <art.nebar@mail.ru>
2022-12-02 11:27:53 +0300

    Initial Commit

---
+++ YouHostels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouHostels
+//
+//  Created by Vladimir Khalin on 02.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
